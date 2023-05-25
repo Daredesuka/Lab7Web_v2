9f7f0a03809ac15ec1a23c05d07c0ef4f8f3a8e0
Daredesuka <Siapahayooh88@gmail.com>
2023-05-25 21:09:56 +0700

    first commit

---
+++ app/Views/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?= $this->include('template/header'); ?>
+<section id="about">
+        <div class="row">
+            <img src="ogie.PNG" title="Veno Setyoaji Wiratama" alt="Veno Setyoaji Wiratama" class="image-circle" width="200" style="float: left; border: 2px solid black;">
+            <h1>Hallo!</h1>
+            <p>Nama saya Ogie Kukuh Yuwono, Saya adalah seorang mahasiswa Universitas Pelita Bangsa Jurusan Teknik Informatika. Saya lahir di Pemalang, 09 April 2003.
+            </p>
+        </div>
+    </section>
+<?= $this->include('template/footer'); ?>
